cd414d9a4f24b5074afc540a4e2315c6560f4fd0
Martin Aguero <martin.aguero84@gmail.com>
2024-04-13 19:26:42 -0500

    edited URL_BASE and the axios.post method with this new url

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //Configuration for TrackHive API (key and base url):
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MTA0YmEwNzVlMjkyMDA3YjkzZGFkOSIsImlhdCI6MTcxMjM0NDc1OX0.KXebc8gmV66x8Z1oCa13AGavRcCYYmxZzf9AfkYNeXU';
+const API_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MTA0YmEwNzVlMjkyMDA3YjkzZGFkOSIsImlhdCI6MTcxMjM0NDc1OX0.KXebc8gmV66x8Z1oCa13AGavRcCYYmxZzf9AfkYNeXU';
 const URL_BASE = 'https://api.trackinghive.com/trackings';
 
 //Function to create a tracking
@@ -14,7 +14,7 @@ export const createTracking = async (trackingNumber, carrierSlug) => {
             'Content-Type': 'application/json'
         });
 
-        const response = await axios.post(`${URL_BASE}`, {
+        const response = await axios.post(URL_BASE, {
             tracking_number: trackingNumber,
             slug: carrierSlug
         }, {
