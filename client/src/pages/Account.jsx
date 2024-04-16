246a0378b2cf4ca6d6ffa0ce99d6a35534ca8fa6
Martin Aguero <martin.aguero84@gmail.com>
2024-04-15 19:51:42 -0500

    created Account page component to manage user account settings.

---
+++ client/src/pages/Account.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import Auth from "../utils/auth";
+
+const Account = () => {
+
+    return (
+        <h1>My Account</h1>
+    );
+};
+
+export default Account;
