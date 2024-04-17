7793641bdae0f66ca33a3ed60630548cf682f2ee
Martin Aguero <martin.aguero84@gmail.com>
2024-04-16 19:27:27 -0500

    adding edit functionality to the account page user fields.  Added functions to handle edit, save, and change.

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useQuery, useMutation } from "@apollo/client";
-import { GET_ME } from "../utils/queries";
+import { GET_ME, UPDATE_USER } from "../utils/queries";
 import Auth from "../utils/auth";
 
 import {
@@ -11,10 +11,11 @@ import {
     Button,
     Flex
 } from "@chakra-ui/react";
-//import { EditIcon, CheckIcon } from "@chakra-ui/react";
+import { EditIcon, CheckIcon } from "@chakra-ui/react";
 
 const Account = () => {
     const { loading, data, error } = useQuery(GET_ME);
+    const [updateUser] = useMutation(UPDATE_USER);
     const [editMode, setEditMode] = useState({ username: false, email: false, password: false });
     const [formData, setFormData] = useState({
         username: '',
@@ -32,6 +33,31 @@ const Account = () => {
             });
         }
     }, [data]);
+
+    const handleEdit = (field) => {
+        setEditMode(prev => ({ ...prev, [field]: true}));
+    };
+
+    const handleSave = async (field) => {
+        setEditMode(prev => ({ ...prev, [field]: false }));
+        try {
+            await updateUser({
+                variables: {
+                    id: data.me._id,
+                    username: formData.username,
+                    email: formData.email,
+                }
+            });
+            console.log('Data updated successfully !');
+        } catch (error) {
+            console.error('Error updating user:', error);
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+    };
 
     if (loading) return <Text>Loading...</Text>;
     if (error) return <Text>Error loading your data!</Text>;
