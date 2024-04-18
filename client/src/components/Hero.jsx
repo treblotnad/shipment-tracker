21c5bc147f49d93b0133394c85de9a61315dc84e
Vik Boyechko <vik.boyechko@gmail.com>
2024-04-18 15:04:29 -0500

    signup modal on hero works, needs styling

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,4 @@
-import SignupForm from './SignupForm'
-import { useState } from 'react'
+import { useState } from 'react';
 import {
     Container,
     Stack,
@@ -9,15 +8,16 @@ import {
     Text,
     Button,
     Image,
-    // Icon,
-    // IconButton,
-    // createIcon,
-    // IconProps,
-    // useColorModeValue,
-} from '@chakra-ui/react'
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react';
+import SignupForm from './SignupForm';
 
 export default function Hero() {
-
     const [showSignupModal, setShowSignupModal] = useState(false);
 
     const handleSignupModalOpen = () => setShowSignupModal(true);
@@ -75,17 +75,6 @@ export default function Hero() {
                         >
                             Sign Up
                         </Button>
-
-                        {/* <Button
-                            rounded={'full'}
-                            size={'lg'}
-                            fontWeight={'normal'}
-                            px={6} >
-                            How It Works
-                        </Button> */}
-                        {/* <Image src={'../public/images/usps.png'} width='120px' height='100%' />
-                        <Image src={'../public/images/ups.png'} width='120px' height='100%' />
-                        <Image src={'../public/images/fedex.png'} width='120px' height='100%' /> */}
                     </Stack>
 
                 </Stack>
@@ -95,55 +84,31 @@ export default function Hero() {
                     align={'center'}
                     position={'relative'}
                     w={'full'}>
-                    {/* <Blob
-                        w={'150%'}
-                        h={'150%'}
-                        position={'absolute'}
-                        top={'-20%'}
-                        left={0}
-                        zIndex={-1}
-                        color={useColorModeValue('red.50', 'red.400')}
-                    /> */}
                     <Box
                         position={'relative'}
                         height={'300px'}
-                        // rounded={'2xl'}
-                        // boxShadow={'2xl'}
                         width={'full'}
                         overflow={'hidden'}>
-
                         <Image
                             alt={'Hero Image'}
                             fit={'cover'}
                             align={'center'}
-                            // w={'100%'}
                             h={'100%'}
-                            src={
-                                '../images/heroscreen.png'
-                            }
+                            src={'../images/heroscreen.png'}
                         />
                     </Box>
                 </Flex>
             </Stack>
+            <Modal isOpen={showSignupModal} onClose={handleSignupModalClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Sign Up</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <SignupForm />
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
         </Container >
     )
 }
-
-
-// const Blob = (props: IconProps) => {
-//     return (
-//         <Icon
-//             width={'100%'}
-//             viewBox="0 0 578 440"
-//             fill="none"
-//             xmlns="http://www.w3.org/2000/svg"
-//             {...props}>
-//             <path
-//                 fillRule="evenodd"
-//                 clipRule="evenodd"
-//                 d="M239.184 439.443c-55.13-5.419-110.241-21.365-151.074-58.767C42.307 338.722-7.478 282.729.938 221.217c8.433-61.644 78.896-91.048 126.871-130.712 34.337-28.388 70.198-51.348 112.004-66.78C282.34 8.024 325.382-3.369 370.518.904c54.019 5.115 112.774 10.886 150.881 49.482 39.916 40.427 49.421 100.753 53.385 157.402 4.13 59.015 11.255 128.44-30.444 170.44-41.383 41.683-111.6 19.106-169.213 30.663-46.68 9.364-88.56 35.21-135.943 30.551z"
-//                 fill="currentColor"
-//             />
-//         </Icon>
-//     )
-// }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,95 +1,53 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
-import SignUpForm from "./SignupForm";
-import LoginForm from "./LoginForm";
-import Auth from "../utils/auth";
-import NavLogo from "./Logo";
-import { Box, Text, Button } from "@chakra-ui/react";
-import { useQuery } from "@apollo/client";
-import { GET_ME } from "../utils/queries";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Text, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from '@chakra-ui/react';
+import SignUpForm from './SignupForm';
+import LoginForm from './LoginForm';
+import Auth from '../utils/auth';
+import NavLogo from './Logo';
+import { useQuery } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
 
 const AppNavbar = () => {
-    // set modal display state
     const [showModal, setShowModal] = useState(false);
-    // Gets the user's data
     const { loading, data, error } = useQuery(GET_ME);
-    if (loading) {
-        return (
-            <>
-                <Box h="85px"></Box>
-            </>
-        );
-    }
-    console.log(data);
 
     return (
         <>
-            <Navbar expand="lg">
-                <Container fluid>
-                    <NavLogo as={Link} to="/" w="200px"></NavLogo>
-
-                    <Navbar id="navbar" className="d-flex flex-row-reverse">
-                        <Nav className="d-flex">
+            <Box as="nav" p={4} bg="blue.500" color="white">
+                <Link to="/" style={{ marginRight: '20px' }}>
+                    <NavLogo w="200px" />
+                </Link>
+                <Box ml="auto">
+                    {loading ? (
+                        <Text>Loading...</Text>
+                    ) : (
+                        <>
                             {Auth.loggedIn() ? (
                                 <>
-                                    <Text
-                                        px={2}
-                                        paddingRight="30px"
-                                        paddingTop="5px"
-                                        textColor="grey"
-                                        fontSize="19px"
-                                    >
+                                    <Text px={2} paddingRight="30px" paddingTop="5px" textColor="grey" fontSize="19px">
                                         Hello, {data?.me?.firstname}
                                     </Text>
-                                    <Nav.Link as={Link} to="/dashboard">
-                                        Dashboard
-                                    </Nav.Link>
-                                    <Nav.Link as={Link} to="/account">
-                                        Account
-                                    </Nav.Link>
+                                    <Link to="/dashboard" style={{ marginRight: '10px' }}>Dashboard</Link>
+                                    <Link to="/account" style={{ marginRight: '10px' }}>Account</Link>
                                     <Button onClick={Auth.logout}>Logout</Button>
                                 </>
                             ) : (
                                 <Button onClick={() => setShowModal(true)}>Login</Button>
-                                // <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
                             )}
-                        </Nav>
-                    </Navbar>
-                </Container>
-            </Navbar>
-            {/* set modal data up */}
-            <Modal
-                size="lg"
-                show={showModal}
-                onHide={() => setShowModal(false)}
-                aria-labelledby="signup-modal"
-            >
-                {/* tab container to do either signup or login component */}
-                <Tab.Container defaultActiveKey="login">
-                    {/* <Modal.Header closeButton>
-                        <Modal.Title id="signup-modal">
-                            <Nav variant="pills">
-                                <Nav.Item>
-                                    <Nav.Link eventKey="login">Login</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="signup">Sign Up</Nav.Link>
-                                </Nav.Item>
-                            </Nav>
-                        </Modal.Title>
-                    </Modal.Header> */}
-                    <Modal.Body>
-                        <Tab.Content>
-                            <Tab.Pane eventKey="login">
-                                <LoginForm handleModalClose={() => setShowModal(false)} />
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="signup">
-                                <SignUpForm handleModalClose={() => setShowModal(false)} />
-                            </Tab.Pane>
-                        </Tab.Content>
-                    </Modal.Body>
-                </Tab.Container>
+                        </>
+                    )}
+                </Box>
+            </Box>
+            <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Login</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <LoginForm handleModalClose={() => setShowModal(false)} />
+                    </ModalBody>
+                </ModalContent>
             </Modal>
         </>
     );
